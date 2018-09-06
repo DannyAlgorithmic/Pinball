a7de5a77143acb899d127872bb5a4785bc1f05a1
Danny Algorithmic <38365455+DannyAlgorithmic@users.noreply.github.com>
2018-09-06 17:54:48 -0400

    Update Bal Physics
    Updated gravity modifiers and added trail renderer

---
+++ Assets/_Scripts/BallGravityModifier.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGravityModifier : MonoBehaviour {
+
+    [Range(0f, 4f)]
+    public float GravityModifierAmount = 1.0f;
+
+    Rigidbody2D body;
+
+    float startGravity;
+    private bool PlayerOneSide = true;
+
+    private void OnEnable()
+    {
+        body = GetComponent<Rigidbody2D>();
+
+        startGravity = body.gravityScale;
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerOneSide = body.position.y <= 0;
+        GravityModifier();
+        CheckForGravitySwitch();
+    }
+
+    private void GravityModifier(){
+        if(PlayerOneSide == true){
+            if(body.velocity.y < 0.0f)
+                body.velocity += Vector2.up * Physics2D.gravity.y * (GravityModifierAmount - 1.0f) * Time.fixedDeltaTime;
+        }else{
+            if(body.velocity.y > 0.0f)
+                body.velocity += Vector2.down * Physics2D.gravity.y * (GravityModifierAmount - 1.0f) * Time.fixedDeltaTime;
+        }
+    }
+
+
+
+    private void CheckForGravitySwitch(){
+        if (PlayerOneSide)
+        {
+            if (Mathf.Approximately(body.gravityScale, startGravity) == false)
+            { GravitySwitch( startGravity ); }
+        }
+        else
+        {
+            if (Mathf.Approximately(body.gravityScale, -startGravity) == false)
+            { GravitySwitch( -startGravity ); }
+        }
+    }
+
+    public void GravitySwitch(float _g)
+    { body.gravityScale = _g; }
+}
---
+++ Assets/_Scripts/Respawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+	private Vector2 StartPosition;
+	private Rigidbody2D body;
+
+	private void OnEnable(){
+		StartPosition = transform.position;
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D col){
+		if(col.CompareTag("Respawner")){
+			body.position = StartPosition;
+			body.velocity = Vector2.zero;
+			// Debug.Log("Respawned");
+		}
+	}
+}
