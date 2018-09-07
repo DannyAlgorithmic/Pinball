54d60dccc48d4589525a33b95f2b4a92fd97b926
Danny Algorithmic <38365455+DannyAlgorithmic@users.noreply.github.com>
2018-09-06 21:08:10 -0400

    Code

---
+++ Assets/AudioScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour {
+
+	public AudioClip PlaceholderClip;
+	public string CollisionTag = "";
+
+	private AudioSource SoundSource;
+
+	private void OnEnable(){
+		SoundSource = GetComponent<AudioSource>();
+	}
+	
+	private void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.CompareTag(CollisionTag) == true)
+			SoundSource.PlayOneShot(PlaceholderClip);
+	}
+
+}
--- Assets/_Scripts/Respawn.cs
+++ Assets/_Scripts/Respawn.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class Respawn : MonoBehaviour {
 	private Vector2 StartPosition;
 	private Rigidbody2D body;
+	private TrailRenderer trail;
+	float startTrailTime;
 
 	private void OnEnable(){
 		StartPosition = transform.position;
 		body = GetComponent<Rigidbody2D>();
+		trail = GetComponent<TrailRenderer>();
+		startTrailTime = trail.time;
 	}
 
 	private void OnTriggerEnter2D(Collider2D col){
 		if(col.CompareTag("Respawner")){
-			body.position = StartPosition;
 			body.velocity = Vector2.zero;
+			body.isKinematic = true;
+			trail.emitting = false;
+			trail.time = 0;
+			trail.Clear();
+			body.MovePosition(StartPosition);
+			// Debug.Log(trail.positionCount);
 			// Debug.Log("Respawned");
 		}
+	}
+
+	private void OnTriggerExit2D(Collider2D col){
+		if(col.CompareTag("Respawner")){
+			StartCoroutine("ResetTrail", startTrailTime + 0.05);
+		}
+	}
+
+	IEnumerator ResetTrail(float t){
+		yield return new WaitForSecondsRealtime(t);
+		body.isKinematic = false;
+		trail.time = startTrailTime;
+		trail.emitting = true;
 	}
 }
